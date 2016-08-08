c01dbd205940115c4011395e7ee44825df7e3eb1
Dayne Wright <daynewr@gmail.com>
2016-08-08 15:10:28 -0500

    started creating jasmine files

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,13 @@
-var gulp    = require('gulp');
-var jshint  = require('gulp-jshint');
+var gulp = require('gulp');
+var jshint = require('gulp-jshint');
 
- gulp.task('lint', function(){
-    return gulp.src('./src/js/**/*.js')
-           .pipe(jshint())
-           .pipe(jshint.reporter('default'), {verbose: true});
- });
 
- gulp.task('watch', function(){
-    gulp.watch('src/js/**/*.js', ['lint']);
- });
+ gulp.task('lint', () =>
+    gulp.src('./src/js/**/*.js')
+      .pipe(jshint())
+      .pipe(jshint.reporter('default'), {verbose: true})
+ );
+
+ gulp.task('watch', () =>
+    gulp.watch('src/js/**/*.js', ['lint'])
+ );
