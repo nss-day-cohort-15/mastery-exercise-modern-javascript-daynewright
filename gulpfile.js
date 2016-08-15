8b1b46b40546ef9ef1370e507c8b484d0cacaecf
Dayne Wright <daynewr@gmail.com>
2016-08-15 10:44:04 -0500

    updated gulp to force script order on build

--- gulpfile.js
+++ gulpfile.js
@@ -6,6 +6,8 @@ var rename = require('gulp-rename');
 var cleanCSS = require('gulp-clean-css');
 var babel = require('gulp-babel');
 var inject = require('gulp-inject');
+var series = require('stream-series');
+//var removeHtmlComments = require('gulp-remove-html-comments');
 
 
 /////////  ERROR CALLBACK   /////////
@@ -15,21 +17,21 @@ function errorLog(err){
 }
 
 var files = {
- js : {
-				 scripts : 'src/dev/js/**/*.js',
-				 vender  : ['src/lib/jquery/dist/jquery.min.js','src/lib/bootstrap/dist/js/bootstrap.min.js']
-			 },
- css : {
-				 styles : 'src/dev/css/**/*.css',
-				 vender : ['src/lib/bootstrap/dist/css/bootstrap.min.css']
-			 },
- html : {
-				 index : 'src/dev/index.html'
-			 }
+    js: {
+        scripts: 'src/dev/js/**/*.js',
+        vender: ['src/lib/jquery/dist/jquery.min.js', 'src/lib/bootstrap/dist/js/bootstrap.min.js']
+    },
+    css: {
+        styles: 'src/dev/css/**/*.css',
+        vender: ['src/lib/bootstrap/dist/css/bootstrap.min.css']
+    },
+    html: {
+        index: 'src/dev/index.html'
+    }
 }
 
 // Lint task for JS source files
- gulp.task('lint', () =>
+ gulp.task('lint-js', () =>
     gulp.src(files.js.scripts)
       .pipe(jshint({'esversion': 6}))
       .pipe(jshint.reporter('default'), {verbose: true})
@@ -43,7 +45,7 @@ var files = {
       .pipe(uglify())
       .on('error', errorLog)
       .pipe(rename('script.min.js'))
-      .pipe(gulp.dest('src/app/js'))
+      .pipe(gulp.dest('src/build/js'))
  );
 
  gulp.task('concat-uglify-vender-js', () =>
@@ -52,7 +54,7 @@ var files = {
       .pipe(uglify())
       .on('error', errorLog)
       .pipe(rename('vender.min.js'))
-      .pipe(gulp.dest('src/app/js'))
+      .pipe(gulp.dest('src/build/js'))
 );
 
 gulp.task('concat-uglify-styles-css', () =>
@@ -61,7 +63,7 @@ gulp.task('concat-uglify-styles-css', () =>
       .pipe(cleanCSS({'keepSpecialComments' : 0}))
       .on('error', errorLog)
       .pipe(rename('style.min.css'))
-      .pipe(gulp.dest('src/app/css'))
+      .pipe(gulp.dest('src/build/css'))
 );
 
 gulp.task('concat-uglify-vender-css', () =>
@@ -70,31 +72,45 @@ gulp.task('concat-uglify-vender-css', () =>
       .pipe(cleanCSS({'keepSpecialComments' : 0}))
       .on('error',errorLog)
       .pipe(rename('vender.min.css'))
-      .pipe(gulp.dest('src/app/css'))
+      .pipe(gulp.dest('src/build/css'))
 );
 
 gulp.task('copy-html', () =>
 		gulp.src(files.html.index)
-			.pipe(gulp.dest('src/app'))
+			.pipe(gulp.dest('src/build'))
 );
 
-gulp.task('insert-css-js-links', ['run-js-files', 'run-css-files', 'copy-html'], ()=> {
-		var sources = gulp.src(['src/app/js/*.js','src/app/css/*.css'], {read: false});
+// gulp.task('remove-html-comments', ['insert-css-js-links-copy-html'], () =>
+// 		gulp.src('src/build/*.html')
+// 			.pipe(removeHtmlComments())
+// 	 		.pipe(gulp.dest('src/build'))
+// );
 
-		gulp.src('src/app/index.html')
-			.pipe(inject(sources, {ignorePath: 'src/app/', addRootSlash: false}))
+gulp.task('insert-css-js-links-copy-html', ['run-js-files', 'run-css-files', 'copy-html'], ()=> {
+		var jsMinVender = gulp.src('src/build/js/vender.min.js', {read: false});
+		var jsMinUser = gulp.src('src/build/js/script.min.js', {read: false});
+		var cssMinVender = gulp.src('src/build/css/vender.min.css', {read: false});
+		var cssMinUser = gulp.src('src/build/css/style.min.css', {read: false});
+
+		gulp.src('src/build/index.html')
+			.pipe(inject(series(cssMinVender, cssMinUser), {ignorePath: 'src/build/', addRootSlash: false}))
+			.pipe(inject(series(jsMinVender, jsMinUser), {ignorePath: 'src/build/', addRootSlash: false}))
 			.on('error', errorLog)
-			.pipe(gulp.dest('src/app'));
+			.pipe(gulp.dest('src/build'));
 });
 
 gulp.task('run-js-files', ['concat-uglify-vender-js', 'concat-uglify-scripts-js']);
 gulp.task('run-css-files', ['concat-uglify-vender-css', 'concat-uglify-styles-css']);
+gulp.task('build-app', ['remove-html-comments']);
 
-
-/////////  RUN TASKS   /////////
- gulp.task('watch', () => {
-   gulp.watch(files.js.scripts, ['lint']);
+/////////  RUN FINAL TASKS   /////////
+gulp.task('watch', () => {
+   gulp.watch(files.js.scripts, ['lint-js']);
 	 gulp.watch('src/app/**/*', ['build']);
- });
+});
 
- gulp.task('build', ['copy-html', 'insert-css-js-links']);
+//lint without watch
+gulp.task('lint', ['lint-js']);
+
+//final build without watch
+gulp.task('build', ['insert-css-js-links-copy-html']);
