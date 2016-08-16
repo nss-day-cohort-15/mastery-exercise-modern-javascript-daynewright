d654070bea4dd96ad2944afc02f58cee97398921
Dayne Wright <daynewr@gmail.com>
2016-08-16 00:04:40 -0500

    added dynamic load of weapon selection based on robot

--- src/dev/js/domevents.js
+++ src/dev/js/domevents.js
@@ -2,10 +2,36 @@
 $(document).ready(function(){
 
   //get selections
-  const playerName = $('#player-name').val();
+  const $playerName = $('#player-name');
+  const $playerRobot = $('select');
+  const $button = $('#create-player');
 
+  $playerRobot.change(getWeapons);
 
+  function getWeapons() {
+    let weaponHTML ='';
+    switch($playerRobot[0].value.split('-')[1]) {
+        case 'ATV':
+            weaponHTML += addOptions(['Crush', 'Jump Attack']);
+            break;
+        case 'Bipedal':
+            weaponHTML += addOptions(['Punch', 'Lazer Beams']);
+            break;
+        case 'Drone':
+            weaponHTML += addOptions(['Fly Attack', 'Propeller Slice']);
+            break;
+    }
+   $($playerRobot[1]).html(weaponHTML);
+  }
 
+  //option helper function
+  function addOptions(valueArray){
+    var holder = '';
+      valueArray.forEach(function(e){
+          holder += `<option value="${e.replace(/\W/g,'')}">${e}</option>`;
+      });
+    return holder;
+  }
 
 
   //get players
@@ -26,7 +52,7 @@ $(document).ready(function(){
         <h4> Model: <span>${e.type.name}</span></h4>
         <h4>Health: <span>${e.health}</span></h4>
         <h4>Weapon: <span>${e.weapon.name}</span></h4>
-      </div>`)
+      </div>`);
   });
 
 
