a3259f66035ae7b1e3bbc69d2a9cb5d4a09a96c3
Dayne Wright <daynewr@gmail.com>
2016-08-17 13:47:45 -0500

    added jasmine testing

---
+++ src/spec/script.spec.js
@@ -0,0 +1,58 @@
+describe("The app should have these:", function () {
+
+  it("an Arena global variable", function () {
+    expect(Arena).toBeDefined();
+  });
+
+  it("a constructor to build a robot", function () {
+    expect(Arena.Robots).toBeDefined();
+  });
+
+  it("an attack prototype method", function () {
+    expect(Arena.Robots.prototype.attack).toBeDefined();
+  });
+});
+
+describe("A Player Robot should return these:", function () {
+  var robot = new Arena.Robots();
+      robot.type = robot.setType('OffRoadATV');
+      robot.weapon = robot.getWeapon(null, 'Crush');
+
+  var comp = new Arena.Robots.Comp();
+
+  it('a robot is created', function(){
+    expect(robot).toBeDefined();
+  });
+
+  it("a robot should have a name", function () {
+    expect(robot.name).toBeDefined();
+  });
+
+  it("a robot should have health", function () {
+    expect(robot.health).toBeDefined();
+  });
+
+  it("a robot should have a weapon", function () {
+    expect(robot.weapon).toBeDefined();
+  });
+});
+
+describe("A Computer Robot should return these:", function () {
+  var comp = new Arena.Robots.Comp();
+
+  it('a computer robot is created', function(){
+    expect(robot).toBeDefined();
+  });
+
+  it("a computer robot should have a name", function () {
+    expect(robot.name).toBeDefined();
+  });
+
+  it("a computer robot should have health", function () {
+    expect(robot.health).toBeDefined();
+  });
+
+  it("a computer robot should have a weapon", function () {
+    expect(robot.weapon).toBeDefined();
+  });
+});
